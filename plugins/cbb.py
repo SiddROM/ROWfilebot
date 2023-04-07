9c5707ffcd229412bdb84312490cebb9041a721f
SiddROM <121415948+SiddROM@users.noreply.github.com>
2023-04-07 17:07:01 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='https://t.me/TheRealmofweebs'>This Channel</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Channel : @TheROWG\n○ Support Group : @TheROWG</b>",
+            text = f"<b>○ Creator : <a href='https://t.me/TheRealmofweebs'>This Channel</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Channel : @TheRealmOfWeebs\n○ Support Group : @TheRealmOfWeebs</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
